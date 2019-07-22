4fc0d6fd3467cabcb85c2ec44b9c90c2e1945199
albert deng <albertdeng@kkbox.com>
2019-07-22 15:20:40 +0800

    init page

---
+++ public/js/ui.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	  $(".hamburg").click(function(){
+	     $('.aside').delay(200).toggleClass("hamburg-open");
+	     $(this).delay(200).toggleClass("cross");
+	    });
+
+
+
+
+
+	    $(".product_title").click(function(){
+	    	if($(this).parent().find(".product_list").hasClass("display_down")){
+	    		$(this).parent().find(".product_list").slideUp(300).removeClass("display_down")
+
+	    	}else{
+	    		 $(this).parent().find(".product_list").slideDown(300).addClass("display_down")
+
+	    	}
+
+	    });
+
+
+	    
+
+
+});
+
+
+
+
+
+
